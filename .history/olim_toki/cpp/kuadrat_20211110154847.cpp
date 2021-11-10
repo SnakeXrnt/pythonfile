f42598319a3dcf96082577f82dcca9a46d560176
SnakeXrnt <ethanbastian11@gmail.com>
2021-11-10 15:50:12 +0700

    November 10, 2021 3:50 PM

---
+++ .history/olim_toki/cpp/kuadrat_20211110154847.cpp
@@ -0,0 +1,9 @@
+
+#include <bits/stdc++.h>
+int a , b, c, x, hasil;
+
+int main() {
+    printf("%lf\n", sqrt(5));
+    printf("%lf\n", round(5.2));
+    printf("%lf\n", round(5.6));
+}
---
+++ .history/olim_toki/cpp/kuadrat_20211110154910.cpp
@@ -0,0 +1,13 @@
+
+#include <bits/stdc++.h>
+int a , b, c, x, hasil;
+
+int main() {
+    printf("%d\n", 2 > 1);
+printf("%d\n", 2 < 1);
+printf("%d\n", 2 == 1);
+printf("%d\n", 2 >= 1);
+printf("%d\n", 1 == 1);
+printf("%d\n", 1 != 1);
+printf("%d\n", 1 != 2);
+}
