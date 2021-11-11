bf548df588eff49fcf082f36d8348e2ab9f29f05
SnakeXrnt <ethanbastian11@gmail.com>
2021-11-11 18:19:13 +0700

    November 11, 2021 6:19 PM

---
+++ plusclass/sqlite/04_bcrypt_primary_key/settings.py
@@ -0,0 +1,16 @@
+
+import sqlite3
+
+class Settings:
+
+	CONN = sqlite3.connect('employee.db')
+	CUR = CONN.cursor()
+
+	MENU = """
+(1) SHOW ALL EMPLOYEES
+(2) ADD NEW EMPLOYEE
+(3) FIND EMPLOYEE
+(4) UPDATE SALARY
+(5) REMOVE EMPLOYEE
+(Q) EXIT.
+"""
--- plusclass/sqlite/04_bycrypt_primarykey/employee.py
+++
@@ -1,7 +0,0 @@
-
-class Employee:
-    
-    def __init__(self,first,last,salary):
-        self.first = first
-        self.last = last
-        self.salary = salary
