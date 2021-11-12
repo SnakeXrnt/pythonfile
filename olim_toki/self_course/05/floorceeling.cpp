2a36ec71d7b7e2a4cbad479558a93f32c5fde33a
SnakeXrnt <ethanbastian11@gmail.com>
2021-11-12 09:27:24 +0700

    November 12, 2021 9:27 AM

---
+++ olim_toki/self_course/05/floorceeling.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(void) {
+	long int c,x;
+	double a;
+	
+	scanf("%lf",&a);
+	
+	if (a == trunc(a)) {
+		a = trunc(a);
+		printf("%.0lf %.0lf\n",a,a);
+	}
+	else if (a<0) {
+		c = trunc(a);
+		x = c-1;
+		printf("%ld %ld\n",x,c);
+	}
+	else if (a>0) {
+		c = trunc(a);
+		x = c+1;
+		printf("%ld %ld\n",c,x);
+	}
+}
