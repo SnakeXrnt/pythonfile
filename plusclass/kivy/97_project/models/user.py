777e123ae54f363e82344274c000c9229e2358ff
SnakeXrnt <ethanbastian11@gmail.com>
2022-01-06 10:51:25 +0700

    January 6, 2022 10:51 AM

---
+++ plusclass/kivy/97_project/models/user.py
@@ -0,0 +1,12 @@
+class User:
+
+	def __init__(self, username, first, last, day, month, year):
+		self.id = None
+		self.username = username
+		self.password = None
+		self.first = first
+		self.last = last
+		self.day = day
+		self.month = month
+		self.year = year
+		self.pic = "assets/img/default.png"
