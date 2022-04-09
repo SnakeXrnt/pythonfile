9c0da4c108f1bbb189c3b7daf94186f55aac9d14
SnakeXrnt <ethanbastian11@gmail.com>
2022-04-09 16:05:00 +0700

    2022-04-09_16-04-58

---
+++ .history/RESPONSI/00_battleship_20220409160457.py
@@ -0,0 +1,11 @@
+import os 
+
+setting_up_board = lambda n : [["O"] * n for i in range(n)]
+
+def printing_board(board):
+    os.system('cls')
+    for row in board:
+        print (" ".join(row))
+        
+def setting_up_ship_location (rows):
+    
