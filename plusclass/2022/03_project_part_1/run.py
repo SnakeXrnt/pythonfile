c60459a6519dabd9689a7d8a8e80c4863b8b2f55
SnakeXrnt <ethanbastian11@gmail.com>
2022-05-20 19:18:59 +0700

    2022-05-20_19-18-59

---
+++ plusclass/2022/03_project_part_1/run.py
@@ -0,0 +1,68 @@
+from flask import Flask, render_template, url_for, redirect
+from flask_wtf import FlaskForm
+
+from wtforms import StringField, PasswordField, SubmitField, BooleanField
+from wtforms.validators import DataRequired, Length, Email, EqualTo
+
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'ILOVEYOU3000'
+
+tweets = [
+	{
+		'author' : 'Budi Doremi',
+		'content' : 'This is my first tweet',
+		'date_posted' : 'May 10, 2022'
+	},
+	{
+		'author' : 'Alex Chandra',
+		'content' : 'Hello World',
+		'date_posted' : 'May 11, 2022'
+	}
+]
+
+
+
+class RegistrationForm(FlaskForm):
+	username = StringField('Username', validators=[DataRequired(), Length(min=2, max=20)])
+	email = StringField('Email', validators=[DataRequired(), Email()])
+	password = PasswordField('Password', validators=[DataRequired()])
+	confirm_password = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password')])
+	submit = SubmitField('Sign Up')
+
+
+class LoginForm(FlaskForm):
+	email = StringField('Email', validators=[DataRequired(), Email()])
+	password = PasswordField('Password', validators=[DataRequired()])
+	remember = BooleanField('Remember Me')
+	submit = SubmitField('Login')
+
+
+@app.route("/") # 127.0.0.1:8000
+@app.route("/home") # 127.0.0.1:8000/home
+def home():
+	return render_template('home.html', tweets=tweets)
+
+@app.route("/register", methods=['GET', 'POST'])
+def register():
+	form = RegistrationForm()
+	return render_template('register.html', title='Register', form=form)
+
+
+@app.route("/login")
+def login():
+	form = LoginForm()
+	return render_template('login.html', title='Login', form=form)
+
+@app.route("/about")
+def about():
+	return render_template('about.html', title='About Us')
+
+
+if __name__ == '__main__':
+	app.run(debug=True)
+
+
+
+
+
